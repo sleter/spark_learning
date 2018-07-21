80691b79c239e2c04617c208d3cbaa0b08003ba4
sleter <sleter97@gmail.com>
2018-07-21 21:54:10 +0200

    rdd

---
+++ friends-by-age.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+lines = sc.textFile("file:///C:/Users/N0tE/Documents/GitHub/spark_learning/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+results = averagesByAge.collect()
+sortedResults =  sorted(results, key=lambda x: x[0])
+for result in sortedResults:
+    print(result)
